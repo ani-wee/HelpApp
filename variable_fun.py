7d28ed7f7324e39a57b202d7862927c392796036
anisaahmad <anisaahmad485@gmail.com>
2026-06-14 00:07:15 -0400

    "Completed variables assignment with input"

---
+++ variable_fun.py
@@ -0,0 +1,26 @@
+first_number = int(input("Please enter the first number\n "))
+second_number = int(input("Please enter the second number\n "))
+answer = first_number * second_number
+print("The answer to your query is", answer)
+third_number = int(input("Please enter the third number\n "))
+fourth_number = int(input("Please enter the fourth number\n "))
+answer = third_number / fourth_number
+print("The answer to your query is", answer)
+fifth_number = int(input("Please enter the fifth number\n "))
+sixth_number = int(input("Please enter the sixth number\n "))
+answer = fifth_number // sixth_number
+print("The answer to your query is", answer)
+seventh_number = int(input("Please enter the seventh number\n "))
+eighth_number = int(input("Please enter the eighth number\n "))
+answer = seventh_number % eighth_number
+print("The answer to your query is", answer)
+ninth_number = int(input("Please enter the ninth number\n "))
+tenth_number = int(input("Please enter the tenth number\n "))
+answer = ninth_number + tenth_number
+print("The answer to your query is", answer)
+eleventh_number = int(input("Please enter the eleventh number\n "))
+twelfth_number = int(input("Please enter the twelth number\n "))
+answer = eleventh_number - twelfth_number
+print("The answer to your query is", answer)
+print("Anisa Ahmad")
+

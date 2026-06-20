1b4573550ae91999c6c35927c8ff0be0eed72ad6
ani-wee <anisaahmad485@gmail.com>
2026-06-20 00:19:03 -0400

    Add files via upload
    Electric charges assignment

---
+++ PyCharmMiscProject/electric_charges.py
@@ -0,0 +1,38 @@
+hours = int(input("Enter the KW hours used:"))
+#test case number 1
+first_rate = 7.633 / 100
+second_rate = 9.259 / 100
+#rate in dollars
+if hours <=1000:
+    amount = (1000 * first_rate)
+else:
+    amount = (1000 * first_rate) + ((hours - 1000) * second_rate)
+print("Amount owed is $", amount, sep="")
+#test case number 2
+hours = int(input("Enter the KW hours used:"))
+first_rate = 7.633 / 100
+second_rate = 9.259 / 100
+if hours <=1000:
+    amount = (1000 * first_rate)
+else:
+    amount = (1000 * first_rate) + ((hours - 1000) * second_rate)
+print("Amount owed is $", amount, sep="")
+#test case number 3
+hours = int(input("Enter the KW hours used:"))
+first_rate = 7.633 / 100
+second_rate = 9.259 / 100
+if hours <=1000:
+    amount = (1000 * first_rate)
+else:
+    amount = (1000 * first_rate) + ((hours - 1000) * second_rate)
+print("Amount owed is $", amount, sep="")
+#test case number 4
+hours = int(input("Enter the KW hours used:"))
+first_rate = 7.633 / 100
+second_rate = 9.259 / 100
+if hours <=1000:
+    amount = (1000 * first_rate)
+else:
+    amount = (1000 * first_rate) + ((hours - 1000) * second_rate)
+print("Amount owed is $", amount, sep="")
+
